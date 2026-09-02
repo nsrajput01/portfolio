e2371a06f0cd5dd5d5ca2b67c958538277cb31bf
Nitesh Singh <singhrajputnitesh937@gmail.com>
2026-09-03 01:01:09 +0530

    modified navbar and hero section

---
+++ script.js
@@ -0,0 +1,66 @@
+ const words = [
+            "Web Applications",
+            "IoT Based projects",
+            "Python Based projects"
+        ];
+
+        let wordIndex = 0;
+        let charIndex = 0;
+        let deleting = false;
+
+        const changingText =
+            document.getElementById("changingText");
+
+
+        function typeWriter() {
+
+            const currentWord = words[wordIndex];
+
+            if (!deleting) {
+
+                // Type the word
+                changingText.textContent =
+                    currentWord.substring(0, charIndex + 1);
+
+                charIndex++;
+
+                // When word is completely typed
+                if (charIndex === currentWord.length) {
+
+                    deleting = true;
+
+                    setTimeout(typeWriter, 1500);
+
+                    return;
+                }
+
+            } else {
+
+                // Delete the word
+                changingText.textContent =
+                    currentWord.substring(0, charIndex - 1);
+
+                charIndex--;
+
+                // When word is completely deleted
+                if (charIndex === 0) {
+
+                    deleting = false;
+
+                    wordIndex++;
+
+                    if (wordIndex === words.length) {
+                        wordIndex = 0;
+                    }
+                }
+            }
+
+            setTimeout(
+                typeWriter,
+                deleting ? 60 : 100
+            );
+        }
+
+
+        // Start typewriter
+        typeWriter();
